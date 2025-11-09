ae7c8a959dd8b1df47c4f6f700a8390224925fb9
z-s-kusz <z.s.kusz@gmail.com>
2025-11-09 15:43:26 -0600

    add report button logic  + psuedo code

--- src/game/ReportControls.svelte
+++ src/game/ReportControls.svelte
@@ -1,27 +1,64 @@
 <script lang="ts">
-  let open = $state(false);
+    import { getActiveRoom, getRooms } from '../GameState.svelte';
+
+    let open = $state(false);
+    let showRoomOptions = $state(false);
+
+    const report = (type: string, roomId?: string) => {
+        if (!roomId) roomId = getActiveRoom().id;
+
+        // if room with id = roomId has an active anomaly of reported type
+            // show fixing screen
+            // if 2 are active handle removing only correct one
+            // else remove and go back to main image
+        // else show no anomoly message
+
+        // finally hide messages
+    };
+
+    const toggleReportMenu = () => {
+        if (open) cancel();
+        else open = true;
+    };
+
+    const cancel = () => {
+        open = false;
+        showRoomOptions = false;
+    };
 </script>
 
-<button type="button" onclick={() => open = true}>Report</button>
+<button type="button" onclick={toggleReportMenu}>Report</button>
 {#if open}
-  <div class="reports">
-    <button type="button">Object Dissapeared</button>
-    <button type="button">Object Appeared</button>
-    <button type="button">Object Moved</button>
-    <button type="button">Image Anomaly</button>
-    <button type="button">Door Anomaly</button>
-    <button type="button">Light Anomaly</button>
-    <button type="button">Camera Malfunction</button>
-    <button type="button">Corpse</button>
-    <button type="button">Intruder</button>
-    <button type="button" onclick={() => open = false}>Cancel Report</button>
-  </div>
+    <div class="reports">
+        <button type="button" onclick={() => report('obj-dpr')}>Object Dissapeared</button>
+        <button type="button" onclick={() => report('obj-apr')}>Object Appeared</button>
+        <button type="button" onclick={() => report('obj-mov')}>Object Moved</button>
+        <button type="button" onclick={() => report('img-aly')}>Image Anomaly</button>
+        <button type="button" onclick={() => report('dor-aly')}>Door Anomaly</button>
+        <button type="button" onclick={() => report('lit-aly')}>Light Anomaly</button>
+        <button type="button" onclick={() => showRoomOptions = true}>Camera Malfunction</button>
+        <button type="button" onclick={() => report('crp-apr')}>Corpse</button>
+        <button type="button" onclick={() => report('itr-apr')}>Intruder</button>
+        <button type="button" onclick={cancel}>Cancel</button>
+    </div>
+{/if}
+
+{#if showRoomOptions}
+    <div>
+        {#each getRooms() as room}
+            <button type="button" onclick={() => report('cam-mal', room.id)}>{room.label}</button>
+        {/each}
+    </div>
 {/if}
 
 <style>
-.reports {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem
-}
+    button {
+        background-color: rgba(0, 0, 0, 0.356);
+        padding: 0.25rem;
+    }
+    .reports {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem
+    }
 </style>
